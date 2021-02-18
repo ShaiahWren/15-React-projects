1e990805f229acb417fa17a7b9479d08ca7d23df
Shaiah Wren <sde2111@columbia.edu>
2021-02-18 11:46:00 -0500

    toggle sidebar

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,36 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
+import logo from "./logo.svg";
+import Sidebar from "./Sidebar";
+import Social from "./Social";
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  const [showLinks, setShowLinks] = useState(false);
 
-export default Navbar
+  const handleClick = () => {
+    setShowLinks(!showLinks);
+  };
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" />
+          <button className="nav-toggle" onClick={handleClick}>
+            <FaBars />
+          </button>
+        </div>
+       
+          <div className={
+            `${showLinks ? 'links-container show-container' : 'links-container'}`}>
+            <Sidebar />
+          </div>
+       
+
+        <Social />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
