cd602932099c84f91f781d825580ad1b5f7610d6
Shaiah Wren <sde2111@columbia.edu>
2021-03-05 15:10:13 -0500

    finish stripe project

--- 13-stripe-submenus/setup/src/context.js
+++ 13-stripe-submenus/setup/src/context.js
@@ -5,15 +5,19 @@ const AppContext = React.createContext();
 
 export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
-
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+const [location, setLocation] = useState({})
+const [page, setPage] = useState({page: '', links:[]})
   const openSidebar = () => {
     setIsSidebarOpen(true);
   };
   const closeSidebar = () => {
     setIsSidebarOpen(false);
   };
-  const openSubmenu = () => {
+  const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text);
+    setPage(page)
+    setLocation(coordinates)
     setIsSubmenuOpen(true);
   };
   const closeSubmenu = () => {
@@ -29,6 +33,8 @@ export const AppProvider = ({ children }) => {
         openSidebar,
         closeSubmenu,
         closeSidebar,
+        location,
+        page,
       }}
     >
       {children}
